3b60973619591c1a69f2471f0f410a34fddeb8f5
JorgeDirkx <jorge.dirkx@gmail.com>
2020-06-16 23:04:56 +0200

    UPDATE: update / toReview

---
+++ src/com/jorgedirkx/wrapperclasses/App.java
@@ -0,0 +1,12 @@
+package com.jorgedirkx.wrapperclasses;
+
+public class App {
+    public static void main(String[] args) {
+        //wrappers; primitive data types are wrapped in objects; object edition of each data type
+
+        Integer intObject = Integer.valueOf(5);
+        System.out.println(intObject);
+
+        //autoboxing: datatype to wrapping of object is auto: autoboxing !
+    }
+}
