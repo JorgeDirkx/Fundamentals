693856f6ea88bd979f4de309c22cfed1cdb2fc73
JorgeDirkx <jorge.dirkx@gmail.com>
2020-06-16 22:01:51 +0200

    UPDATE: add Abstract classes

--- src/com/jorgedirkx/exceptions/App.java
+++ src/com/jorgedirkx/exceptions/App.java
@@ -1,4 +1,17 @@
 package com.jorgedirkx.exceptions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.FileHandler;
+
 public class App {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("test.txt");
+
+        FileReader fileReader = new FileReader(file);
+    }
+
+
 }
